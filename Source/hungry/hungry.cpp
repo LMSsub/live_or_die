f2f999a0d3642d365126004d89b0e58725b873b1
LMSsub <lim0492@naver.com>
2025-05-20 17:17:15 +0900

    del
    del

--- Source/hungry/hungry.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "hungry.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, hungry, "hungry" );
- 
